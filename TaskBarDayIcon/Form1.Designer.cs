10eb6d95fdc415ff635589d1bcc15c264e1705cd
Claude Hebert Jr <CJ.Hebert@vermont.gov>
2022-05-04 11:37:23 -0400

    initial

---
+++ TaskBarDayIcon/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace TaskBardDayIcon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.MinimumSize = new System.Drawing.Size(150, 62);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(20);
+            this.label1.Size = new System.Drawing.Size(233, 132);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Task Bar Days - Shows the Day Number - use with Small Tackbar Icons";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 900000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(230, 132);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(250, 175);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TaskBarDays";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TaskBarDayIcon/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskBardDayIcon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            var img = DrawTextImage(DateTime.Now.Day.ToString());
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 900000;
+            timer1.Enabled = true;
+            UpdateIcon();
+        }
+        private void UpdateIcon()
+        {
+            Bitmap bmp = new Bitmap(DrawTextImage(DateTime.Now.Day.ToString()), new Size(32, 32));
+            var hicon = bmp.GetHicon();
+            Icon ico = Icon.FromHandle(hicon);
+            this.Icon = ico;
+        }
+        private Image DrawTextImage(String text)
+        {
+            Font font = Control.DefaultFont;
+            Color textColor = Color.Black;
+            Color backColor = Color.White;
+            Size minSize = Size.Empty;
+            return DrawTextImage(text, font, textColor, backColor, Size.Empty);
+        }
+        private Image DrawTextImage(String text, Font font, Color textColor, Color backColor, Size minSize)
+        {
+            SizeF textSize;
+            using (Image img = new Bitmap(1, 1))
+            {
+                using (Graphics drawing = Graphics.FromImage(img))
+                {
+                    textSize = drawing.MeasureString(text, font);
+                    if (!minSize.IsEmpty)
+                    {
+                        textSize.Width = textSize.Width > minSize.Width ? textSize.Width : minSize.Width;
+                        textSize.Height = textSize.Height > minSize.Height ? textSize.Height : minSize.Height;
+                    }
+                }
+            }
+            Image retImg = new Bitmap((int)textSize.Width, (int)textSize.Height);
+            using (var drawing = Graphics.FromImage(retImg))
+            {
+                drawing.Clear(backColor);
+                using (Brush textBrush = new SolidBrush(textColor))
+                {
+                    drawing.DrawString(text, font, textBrush, 0, 0);
+                    drawing.Save();
+                }
+            }
+            return retImg;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateIcon();
+        }
+    }
+}
